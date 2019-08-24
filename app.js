aaddaf2df265bdd8c17f4fa8076499f104404589
vincemanuel <vincemanuel6@gmail.com>
2019-08-24 13:07:33 +0800

    remove next in app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.static(pathUtil.public));
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
+app.use((req, res) => {
     res.status(404).sendFile(pathUtil.views + '404.html');
 });
 
